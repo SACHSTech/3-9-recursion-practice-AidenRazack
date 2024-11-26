8d454bc1d54a7723be3d1c70c0084ceae480e81a
AidenRazack <aiden.razack25@ycdsbk12.ca>
2024-11-26 00:23:11 -0500

    WIP finished question 2 and 3

---
+++ src/recursion/EvenNumbersInARange.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class EvenNumbersInARange {
+    public static void printEvens(int start, int end) {
+        if (start > end) {
+            return;
+        }
+
+        if (start % 2 == 0) {
+            System.out.println(start);
+            printEvens(start + 2, end);
+        } else {
+            printEvens(start + 1, end);
+        }
+    }
+    public static void main(String[] args) {
+        printEvens(3,13);
+    }
+}
