0d8803c8bb305403d96cc177c04cbdc46983fc36
AidenRazack <aiden.razack25@ycdsbk12.ca>
2024-11-26 10:26:28 -0500

    WIP finished question 10

---
+++ src/recursion/FindMaximumInAnArray.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class FindMaximumInAnArray {
+    public static int findMax(int[] arr, int n) {
+        if (n == 1) {
+            return arr[0];
+        }
+
+        int currentMax = findMax(arr, n - 1);
+        return Math.max(currentMax, arr[n - 1]);
+    }
+    
+    public static void main(String[] args) {
+        int[] array = {1, 5, 3, 9, 2};
+        int n = 3;
+        System.out.println(findMax(array, n));
+    }
+}
+
