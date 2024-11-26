40fcd6e6a32ae5c8dfa3a4b8e41567d4fcbc5016
AidenRazack <aiden.razack25@ycdsbk12.ca>
2024-11-26 00:40:37 -0500

    WIP finished question 6

---
+++ src/recursion/PowerFunction.java
@@ -0,0 +1,15 @@
+package recursion;
+
+public class PowerFunction {
+    public static int power(int base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        }
+
+        return base * power(base, exponent - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(3, 4));
+    }
+}
